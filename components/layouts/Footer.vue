c0dcec8b6120cff077762ef33eff7697ed6dd658
hikarut <ht.hikaru.takahashi@gmail.com>
2019-05-05 21:46:02 +0900

    文言調整

--- components/layouts/Footer.vue
+++ components/layouts/Footer.vue
@@ -1,8 +1,11 @@
 <template>
-  <v-footer class="footer" color="#134b8a" height="200">
+  <v-footer class="footer" color="#134b8a" height="250">
     <v-flex
       text-xs-center class="footer-title">
       <logo /><br>
+      <p class="description">エンジニアのための複業メディアサイト。<br>
+        エンジニアのための技術情報や<br>
+        複業(副業)の情報を提供します。</p>
       <v-btn icon>
         <a :href="twitterUrl" target="blank"><v-icon>fab fa-twitter</v-icon></a>
       </v-btn><br>
@@ -68,5 +71,9 @@ export default {
   margin-top: 10px;
   /* color: black; */
   color: white;
+}
+.description {
+  font-size: 12px;
+  font-weight: bold;
 }
 </style>
