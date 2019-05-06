a211ceba01efc8f8cc56b38996bf5cbf25d01d77
hikarut <ht.hikaru.takahashi@gmail.com>
2019-05-06 16:29:12 +0900

    netlify用に404ページを作成

---
+++ pages/404.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <v-layout align-center column justify-center>
+      <div class="error-word" >ページが見つかりません。</div>
+      <div class="error-top"><nuxt-link to="/">ホーム</nuxt-link>に戻る。</div>
+      <list-item :items="recomendNews" />
+    </v-layout>
+  </div>
+</template>
+
+<script>
+import ListItem from '~/components/organisms/ListItem.vue'
+const recomendNews = require('~/config/recomendNews.json')
+
+export default {
+  components: {
+    ListItem
+  },
+  head() {
+    return {
+      title: 'エラー'
+    }
+  },
+  data: () => ({ recomendNews: recomendNews })
+}
+</script>
+
+<style scoped>
+.error-word {
+  padding-top: 50px;
+}
+.error-top {
+  padding-bottom: 50px;
+}
+</style>
