a2ad596a33eecd65036c05ea1b7c35a8674c3c1c
hikarut <ht.hikaru.takahashi@gmail.com>
2019-05-21 00:16:49 +0900

    debug

--- pages/contact/confirm.vue
+++ pages/contact/confirm.vue
@@ -4,6 +4,7 @@
     method="post"
     data-netlify="true"
     data-netlify-honeypot="bot-field"
+    action="/contact/complete"
   >
     <input type="hidden" name="form-name" value="ask-question" >
     <v-text-field
@@ -25,7 +26,8 @@
       name="contents"
       label="お問い合わせ内容"
     />
-    <v-btn color="success" @click="complete()">送信</v-btn>
+    <!-- <v-btn color="success" @click="complete()">送信</v-btn> -->
+    <v-btn color="success" type="submit">送信</v-btn>
   </form>
 </template>
 
