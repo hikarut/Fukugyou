a1aad697d781e0b4a51e6c507da7af03a1d7549c
hikarut <ht.hikaru.takahashi@gmail.com>
2019-05-21 12:20:09 +0900

    debug

--- pages/contact/index.vue
+++ pages/contact/index.vue
@@ -14,13 +14,13 @@
     />
     <v-text-field
       v-model="form.mail"
-      name="mail"
+      name="mail1"
       label="メールアドレス"
       required
     />
     <v-textarea
       v-model="form.inputText"
-      name="contents"
+      name="contents1"
       label="お問い合わせ内容"
     />
     <v-btn color="success" @click="next()">送信</v-btn>
