8d869751a3ae59f2c4b29e011e866ed0904ef485
hikarut <ht.hikaru.takahashi@gmail.com>
2019-08-04 14:28:33 +0900

    スマホの時に画像を高くする

--- components/atoms/CardImg.vue
+++ components/atoms/CardImg.vue
@@ -1,5 +1,5 @@
 <template>
-  <img v-lazy="src" :alt="alt" src="/loading.png" class="card-img">
+  <img v-lazy="src" :alt="alt" :class="[isBig ? 'card-img-big' : 'card-img']" src="/loading.png">
 </template>
 
 <script>
@@ -12,6 +12,10 @@ export default {
     alt: {
       type: String,
       default: ''
+    },
+    isBig: {
+      type: Boolean,
+      default: false
     }
   }
 }
@@ -21,6 +25,11 @@ export default {
 .card-img {
   width: 100%;
   height: 100px;
+  object-fit: cover;
+}
+.card-img-big {
+  width: 100%;
+  height: 200px;
   object-fit: cover;
 }
 /* PC版は高さを広げる */
