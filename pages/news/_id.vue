a95d5bc9f278ac507b231e03271788f6c4b255fa
hikarut <ht.hikaru.takahashi@gmail.com>
2021-04-21 21:08:55 +0900

    [test] rendering

--- pages/news/_id.vue
+++ pages/news/_id.vue
@@ -1,7 +1,13 @@
 <template>
   <div>
-    <bread-list :items="breadItems"/>
+    <!-- <bread-list :items="breadItems"/> -->
     ニュース詳細
+    <!-- <div v-if="newsDetail === null">
+      <v-progress-linear :indeterminate="true"/>
+    </div>
+    <div v-else>
+      {{ newsDetail }}
+    </div> -->
   </div>
 </template>
 
@@ -13,26 +19,26 @@ export default {
   components: { BreadList },
   data: () => ({}),
   computed: {
-    breadItems() {
-      return [
-        {
-          text: 'ホーム',
-          disabled: false,
-          url: process.env.constant.sitePathHome
-        },
-        {
-          text: process.env.constant.postList,
-          disabled: false,
-          url: process.env.constant.sitePathPosts
-        },
-        {
-          // text: this.post.fields.title.trim(),
-          text: 'aaa',
-          disabled: true,
-          url: process.env.constant.sitePathHome
-        }
-      ]
-    },
+    // breadItems() {
+    //   return [
+    //     {
+    //       text: 'ホーム',
+    //       disabled: false,
+    //       url: process.env.constant.sitePathHome
+    //     },
+    //     {
+    //       text: process.env.constant.postList,
+    //       disabled: false,
+    //       url: process.env.constant.sitePathPosts
+    //     },
+    //     {
+    //       // text: this.post.fields.title.trim(),
+    //       text: 'aaa',
+    //       disabled: true,
+    //       url: process.env.constant.sitePathHome
+    //     }
+    //   ]
+    // },
     ...mapGetters('newsV2', ['newsDetail'])
   },
   async beforeMount() {
