9ebe1c8e9160925f94a419db5ad108fd0528f00a
hikarut <ht.hikaru.takahashi@gmail.com>
2021-04-21 19:53:12 +0900

    [test] rendering

--- pages/news/_id.vue
+++ pages/news/_id.vue
@@ -2,12 +2,6 @@
   <div>
     <bread-list :items="breadItems"/>
     ニュース詳細
-    <!-- <div v-if="newsDetail === null">
-      <v-progress-linear :indeterminate="true"/>
-    </div>
-    <div v-else>
-      {{ newsDetail }}
-    </div> -->
   </div>
 </template>
 
@@ -17,65 +11,8 @@ import BreadList from '~/components/organisms/BreadList.vue'
 
 export default {
   components: { BreadList },
-  head() {
-    return {
-      // title: `${this.newsDetail.title} | ${process.env.constant.title}`,
-      title: `news`,
-      meta: [
-        {
-          hid: 'keywords',
-          name: 'keywords',
-          content: 'aa'
-        },
-        {
-          hid: 'description',
-          name: 'description',
-          content: 'aa'
-        },
-        {
-          hid: 'og:description',
-          property: 'og:description',
-          content: 'aa'
-        },
-        { hid: 'og:url', property: 'og:url', content: this.shareUrl },
-        {
-          hid: 'og:title',
-          property: 'og:title',
-          content: 'aa'
-        },
-        {
-          hid: 'og:image',
-          property: 'og:image',
-          content: 'aa'
-        },
-        {
-          hid: 'twitter:image:src',
-          property: 'twitter:image:src',
-          content: 'aa'
-        }
-      ],
-      script: [
-        {
-          src: 'https://platform.twitter.com/widgets.js',
-          defer: true,
-          async: true
-          // body: true
-        }
-      ],
-      link: [
-        {
-          rel: 'canonical',
-          href: this.shareUrl
-        }
-      ]
-    }
-  },
   data: () => ({}),
   computed: {
-    shareUrl() {
-      // return `${process.env.constant.url}/posts/${this.post.fields.url}/`
-      return `${process.env.constant.url}`
-    },
     breadItems() {
       return [
         {
@@ -95,33 +32,6 @@ export default {
           url: process.env.constant.sitePathHome
         }
       ]
-    },
-    ldJson() {
-      return JSON.stringify({
-        '@context': 'https://schema.org',
-        '@type': 'BlogPosting',
-        mainEntityOfPage: {
-          '@type': 'WebPage',
-          // '@id': `${process.env.constant.url}/posts/${this.post.fields.url}`
-          '@id': `${process.env.constant.url}/news/`
-        },
-        // headline: this.post.fields.title,
-        headline: 'aaa',
-        description: process.env.constant.description,
-        image: {
-          '@type': 'ImageObject',
-          // url: this.post.fields.image.fields.file.url,
-          url: 'aaa',
-          width: 1200,
-          height: 800
-        },
-        author: {
-          '@type': 'Person',
-          name: 'hikaru takahashi'
-        }
-        // datePublished: this.dateString(this.post.sys.createdAt, true),
-        // dateModified: this.dateString(this.post.sys.updatedAt, true)
-      })
     },
     ...mapGetters('newsV2', ['newsDetail'])
   },
